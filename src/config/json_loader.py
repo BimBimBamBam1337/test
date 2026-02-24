f01501e771fdce6b9feb85dd569fb0bbf94f5280
BimBimBamBam1337 <vladimir@MacBook-Air-Vladimir.local>
2026-02-24 23:00:03 +0300

    fear: updat json loader

--- src/config/json_loader.py
+++ src/config/json_loader.py
@@ -1,10 +1,29 @@
 import json
+from dataclasses import dataclass
+
 from src.constants import BASE_DIR
 
 
-def load_json(filename: str) -> dict:
-    with open(BASE_DIR / filename, "r", encoding="utf-8") as f:
-        return json.load(f)
+@dataclass
+class Message:
+    start_message: str
+    access_denied: str
+    join_request: str
+    message_deleted: str
+    message_edited: str
 
+    @classmethod
+    def create(cls, filename: str = "messages.json") -> "Message":
+        messages = cls.load_json(filename)
+        return cls(
+            start_message=messages.get("start_message"),
+            access_denied=messages.get("access_denied"),
+            join_request=messages.get("join_request"),
+            message_deleted=messages.get("message_deleted"),
+            message_edited=messages.get("message_edited"),
+        )
 
-MESSAGES = load_json("messages.json")
+    @staticmethod
+    def load_json(filename: str = "messages.json") -> dict:
+        with open(BASE_DIR / filename, "r", encoding="utf-8") as f:
+            return json.load(f)
